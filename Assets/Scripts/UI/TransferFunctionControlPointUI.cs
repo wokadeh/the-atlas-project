25293d364f5b600b8049b6224d6b5afce5e48476
Jan Storm <jan.storm@study.hs-duesseldorf.de>
2019-05-23 16:43:26 +0200

    Add basis for transfer function ui

---
+++ Assets/Scripts/UI/TransferFunctionControlPointUI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TransferFunctionControlPointUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    [SerializeField] private Color m_HighlightColor;
+
+    private TransferFunctionUI m_TransferFunctionUI;
+    private RectTransform m_RectTransform;
+    private Image m_Image;
+    private Color m_NormalColor;
+    private bool m_IsDragging;
+
+    private void Start() {
+        m_RectTransform = GetComponent<RectTransform>();
+        m_Image = GetComponent<Image>();
+        m_NormalColor = m_Image.color;
+    }
+
+    public void Init(TransferFunctionUI transferFunctionUI) {
+        m_TransferFunctionUI = transferFunctionUI;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        m_Image.color = m_HighlightColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        if (!m_IsDragging) {
+            m_Image.color = m_NormalColor;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        if (eventData.button == PointerEventData.InputButton.Left) {
+            m_TransferFunctionUI.SelectPoint(this);
+        } else if (eventData.button == PointerEventData.InputButton.Right) {
+            m_TransferFunctionUI.DeletePoint(this);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        m_IsDragging = true;
+        m_Image.color = m_HighlightColor;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        m_RectTransform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        m_IsDragging = false;
+
+        if (!RectTransformUtility.RectangleContainsScreenPoint(m_RectTransform, eventData.position)) {
+            m_Image.color = m_NormalColor;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TransferFunctionUI.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TransferFunctionUI : MonoBehaviour, IPointerClickHandler {
+    [SerializeField] private TransferFunctionControlPointUI m_ControlPointPrefab;
+
+    private List<TransferFunctionControlPointUI> m_ControlPoints;
+    private RectTransform m_RectTransform;
+    private TransferFunctionControlPointUI m_SelectedPoint;
+
+    private void Start() {
+        m_ControlPoints = new List<TransferFunctionControlPointUI>();
+        m_RectTransform = GetComponent<RectTransform>();
+
+        GenerateRandomControlPoints();
+    }
+
+    public void SelectPoint(TransferFunctionControlPointUI point) {
+        m_SelectedPoint = point;
+    }
+
+    public void DeletePoint(TransferFunctionControlPointUI point) {
+        Destroy(point.gameObject);
+        m_ControlPoints.Remove(point);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        if (eventData.button == PointerEventData.InputButton.Left) {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(m_RectTransform, eventData.position, null, out Vector2 localPoint);
+            CreatePoint(localPoint);
+        }
+    }
+
+    private void GenerateRandomControlPoints() {
+        Bounds bounds = RectTransformUtility.CalculateRelativeRectTransformBounds(m_RectTransform);
+
+        // HACK: Hardcoded control point size
+        const int NUMBER_OF_POINTS = 10;
+        const int CONTROL_POINT_SIZE = 16;
+
+        float xStep = (bounds.size.x - CONTROL_POINT_SIZE) / NUMBER_OF_POINTS;
+        float x = 0;
+        for (int i = 0; i < NUMBER_OF_POINTS; i++) {
+            float y = Random.Range(0, bounds.size.y - CONTROL_POINT_SIZE);
+
+            CreatePoint(new Vector2(x - bounds.extents.x, y - bounds.extents.y));
+
+            x += xStep;
+        }
+    }
+
+    private void CreatePoint(Vector2 position) {
+        TransferFunctionControlPointUI point = Instantiate(m_ControlPointPrefab, transform);
+        point.GetComponent<RectTransform>().anchoredPosition = position;
+        point.name = $"Transferfunction_Control_Point";
+
+        point.Init(this);
+
+        m_ControlPoints.Add(point);
+    }
+
+    private void ClearPoints() {
+        foreach (var point in m_ControlPoints) {
+            Destroy(point.gameObject);
+        }
+        m_ControlPoints.Clear();
+    }
+}
