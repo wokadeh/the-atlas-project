00579b49b48fa754fd5ee3b5804dc1f65cef98b5
Jan Storm <jan.storm@study.hs-duesseldorf.de>
2019-05-28 20:06:26 +0200

    Add dummy import data handler

---
+++ Assets/Scripts/UI/MenuBar/MenuBarUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using SFB;
+
+public class MenuBarUI : MonoBehaviour {
+    [SerializeField] private Button m_ImportDataButton;
+
+    private void Start() {
+        m_ImportDataButton.onClick.AddListener(DummyLoadData);
+    }
+
+    private void DummyLoadData() {
+        string[] folders = StandaloneFileBrowser.OpenFolderPanel(null, null, false);
+        if (folders.Length == 1) {
+            string folder = folders[0];
+            Debug.Log($"Import data from folder: {folder}");
+            // TODO: Will call some sort of data loader
+            // IDataLoader dataLoader = new DataLoader();
+            // dataLoader.Load(folder);
+        }
+    }
+}
