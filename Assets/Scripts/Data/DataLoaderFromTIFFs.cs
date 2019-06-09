cbf38928e59b1e9243d1297bc6cad15ff2ada9d0
Jan Storm <jan.storm@study.hs-duesseldorf.de>
2019-06-09 13:02:38 +0200

    Further reduce memory usage when loading tiffs

--- Assets/Scripts/Data/DataLoaderFromTIFFs.cs
+++ Assets/Scripts/Data/DataLoaderFromTIFFs.cs
@@ -25,9 +25,9 @@ public class DataLoaderFromTIFFs : IDataLoader {
         }
 
         // We only search in the top directory for files with the ".tif" or ".tiff" ending
-        List<string> paths = null;
+        string[] paths = null;
         try { 
-            paths = Directory.GetFiles(path, "*.*").Where(p => p.EndsWith(".tif") || p.EndsWith(".tiff")).OrderBy(s => PadNumbers(s)).ToList();
+            paths = Directory.GetFiles(path, "*.*").Where(p => p.EndsWith(".tif") || p.EndsWith(".tiff")).OrderBy(s => PadNumbers(s)).ToArray();
         } catch(Exception e) {
             Debug.LogError($"[DataLoaderFromDisk] - Failed to get files from directory: {path} with exception:\n{e.GetType().Name} - {e.Message}!");
             return null;
@@ -35,22 +35,35 @@ public class DataLoaderFromTIFFs : IDataLoader {
 
         // Load in each file as a texture
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-        int count = paths.Count();
-        Texture2D[] textures = new Texture2D[count];
-        for (int i = 0; i < count; i++) {
-            try {
-                textures[i] = LoadTiff(paths[i]);
-            } catch (Exception e) {
-                Debug.LogError($"[DataLoaderFromDisk] - Failed to load tiff at paht: {path} with exception:\n{e.GetType().Name} - {e.Message}!");
-                return null;
-            }
-        }
+        Texture2D[] textures = LoadTiffs(paths);
         Debug.Log($"[DataLoaderFromDisk] - Loading of tiffs took {(stopwatch.ElapsedMilliseconds / 1000.0f).ToString("0.00")} seconds.");
 
         return textures;
     }
 
-    private Texture2D LoadTiff(string path) {
+    private Texture2D[] LoadTiffs(string[] paths) {
+        int length = paths.Length;
+        Texture2D[] textures = new Texture2D[length];
+        
+        // HACK: For now we are only loading tiffs with a certain hardcoded width and height
+        // To reduce memory usage we create the buffers here and reuse them for each load
+        // which works because every image is the same size
+        int[] raster = new int[TEXTURE_WIDTH * TEXTURE_HEIGHT];
+        Color32[] colors = new Color32[TEXTURE_WIDTH * TEXTURE_HEIGHT];
+
+        for (int i = 0; i < length; i++) {
+            string path = paths[i];
+            try {
+                textures[i] = LoadTiff(path, raster, colors);
+            } catch (Exception e) {
+                Debug.LogError($"[DataLoaderFromDisk] - Failed to load tiff at paht: {path} with exception:\n{e.GetType().Name} - {e.Message}!");
+                return null;
+            }
+        }
+        return textures;
+    }
+
+    private Texture2D LoadTiff(string path, int[] raster, Color32[] colors) {
         using (Tiff image = Tiff.Open(path, "r")) {
             // Find the width and height of the image
             FieldValue[] value = image.GetField(TiffTag.IMAGEWIDTH);
@@ -59,7 +72,6 @@ public class DataLoaderFromTIFFs : IDataLoader {
             value = image.GetField(TiffTag.IMAGELENGTH);
             int height = value[0].ToInt();
             
-            // HACK: For now we are only loading tiffs with a certain hardcoded with and height
             if (width != TEXTURE_WIDTH) {
                 throw new TiffException($"Tiff does not have the expected width of: {TEXTURE_WIDTH}");
             }
@@ -67,14 +79,12 @@ public class DataLoaderFromTIFFs : IDataLoader {
                 throw new TiffException($"Tiff does not have the expected height of: {TEXTURE_HEIGHT}");
             }
 
-            // Read the image into the memory buffer
-            int[] raster = new int[width * height];
+            // Read the image into the raster buffer
             if (!image.ReadRGBAImage(width, height, raster)) {
                 throw new TiffException("Failed to read pixels from tiff!");
             }
 
             // We convert the raster to colors
-            Color32[] colors = new Color32[width * height];
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
                     int index = y * width + x;
