7de0237bce34804e28bd8b559ae4a0e6c34de299
Jan Storm <jan.storm@study.hs-duesseldorf.de>
2019-06-12 23:18:59 +0200

    Add proper starting transfer function

--- Assets/Scripts/TransferFunction/Transferfunction.cs
+++ Assets/Scripts/TransferFunction/Transferfunction.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Transferfunction : MonoBehaviour
-{
-
+public class TransferFunction {
     public List<TFColourControlPoint> colourControlPoints = new List<TFColourControlPoint>();
     public List<TFAlphaControlPoint> alphaControlPoints = new List<TFAlphaControlPoint>();
+
+    public Texture2D histogramTexture = null;
 
     private Texture2D texture = null;
     Color[] tfCols;
@@ -14,53 +14,27 @@ public class Transferfunction : MonoBehaviour
     private const int TEXTURE_WIDTH = 512;
     private const int TEXTURE_HEIGHT = 2;
 
-    public Transferfunction()
-    {
+    public TransferFunction() {
         texture = new Texture2D(TEXTURE_WIDTH, TEXTURE_HEIGHT, TextureFormat.RGBAFloat, false);
         tfCols = new Color[TEXTURE_WIDTH * TEXTURE_HEIGHT];
     }
-    // Start is called before the first frame update
-    void Start()
-    {
 
-        Transferfunction function = new Transferfunction();
-        function.AddControlPoint(new TFColourControlPoint(0.0f, new Color(0.6f, 0.6f, 0.1f, 0.1f)));
-        function.AddControlPoint(new TFColourControlPoint(0.25f, new Color(0.6f, 0.4f, 0.13f, 0.4f)));
-        function.AddControlPoint(new TFColourControlPoint(0.5f, new Color(0.3f, 0.3f, 0.0f, 0.3f)));
-        function.AddControlPoint(new TFColourControlPoint(0.8f, new Color(0.3f, 0.5f, 0.0f, 0.1f)));
-        function.AddControlPoint(new TFColourControlPoint(1.0f, new Color(0.3f, 0.1f, 0.0f, 0.1f)));
-
-        function.AddControlPoint(new TFAlphaControlPoint(0.0f, 0.5f));
-        function.AddControlPoint(new TFAlphaControlPoint(0.5f, 0.155f));
-        function.AddControlPoint(new TFAlphaControlPoint(1.0f, 0.4f));
-
-        // function.GenerateTexture();
-        Texture2D transferTexture = function.GetTexture();
-
-        GetComponent<Renderer>().material.SetTexture("_TFTex", transferTexture);
-        Debug.Log(transferTexture);
-    }
-
-    public void AddControlPoint(TFColourControlPoint ctrlPoint)
-    {
+    public void AddControlPoint(TFColourControlPoint ctrlPoint) {
         colourControlPoints.Add(ctrlPoint);
     }
 
-    public void AddControlPoint(TFAlphaControlPoint ctrlPoint)
-    {
+    public void AddControlPoint(TFAlphaControlPoint ctrlPoint) {
         alphaControlPoints.Add(ctrlPoint);
     }
 
-    public Texture2D GetTexture()
-    {
+    public Texture2D GetTexture() {
         if (texture == null)
             GenerateTexture();
 
         return texture;
     }
 
-    public void GenerateTexture()
-    {
+    public void GenerateTexture() {
         List<TFColourControlPoint> cols = new List<TFColourControlPoint>(colourControlPoints);
         List<TFAlphaControlPoint> alphas = new List<TFAlphaControlPoint>(alphaControlPoints);
 
@@ -68,68 +42,48 @@ public class Transferfunction : MonoBehaviour
         cols.Sort((a, b) => (a.dataValue.CompareTo(b.dataValue)));
         alphas.Sort((a, b) => (a.dataValue.CompareTo(b.dataValue)));
 
-        // Add colour points at beginning and end 
+        // Add colour points at beginning and end
         if (cols.Count == 0 || cols[cols.Count - 1].dataValue < 1.0f)
             cols.Add(new TFColourControlPoint(1.0f, Color.white));
         if (cols[0].dataValue > 0.0f)
             cols.Insert(0, new TFColourControlPoint(0.0f, Color.white));
 
-        //Add alpha points at beginning and end
+        // Add alpha points at beginning and end
         if (alphas.Count == 0 || alphas[alphas.Count - 1].dataValue < 1.0f)
             alphas.Add(new TFAlphaControlPoint(1.0f, 1.0f));
         if (alphas[0].dataValue > 0.0f)
             alphas.Insert(0, new TFAlphaControlPoint(0.0f, 0.0f));
-        // Anzahl colours
+
         int numColours = cols.Count;
-        //Anzahl der transparenten
         int numAlphas = alphas.Count;
         int iCurrColour = 0;
         int iCurrAlpha = 0;
 
-
-        for (int iX = 0; iX < TEXTURE_WIDTH; iX++)
-        {
-            //position innerhalb der samples
+        for (int iX = 0; iX < TEXTURE_WIDTH; iX++) {
             float t = iX / (float)(TEXTURE_WIDTH - 1);
-
             while (iCurrColour < numColours - 2 && cols[iCurrColour + 1].dataValue < t)
                 iCurrColour++;
             while (iCurrAlpha < numAlphas - 2 && alphas[iCurrAlpha + 1].dataValue < t)
                 iCurrAlpha++;
-
 
             TFColourControlPoint leftCol = cols[iCurrColour];
             TFColourControlPoint rightCol = cols[iCurrColour + 1];
             TFAlphaControlPoint leftAlpha = alphas[iCurrAlpha];
             TFAlphaControlPoint rightAlpha = alphas[iCurrAlpha + 1];
 
-
             float tCol = (Mathf.Clamp(t, leftCol.dataValue, rightCol.dataValue) - leftCol.dataValue) / (rightCol.dataValue - leftCol.dataValue);
             float tAlpha = (Mathf.Clamp(t, leftAlpha.dataValue, rightAlpha.dataValue) - leftAlpha.dataValue) / (rightAlpha.dataValue - leftAlpha.dataValue);
 
+            Color pixCol = rightCol.colourValue * tCol + leftCol.colourValue * (1.0f - tCol);
+            pixCol.a = rightAlpha.alphaValue * tAlpha + leftAlpha.alphaValue * (1.0f - tAlpha);
 
-            // Farbwerte u. transparenzwerte
-         //   Color pixCol = rightCol.colourValue * tCol + leftCol.colourValue * (1.0f - tCol);
-           // pixCol.a = rightAlpha.alphaValue * tCol + leftAlpha.alphaValue * (1.0f - tCol);
-            Color pixCol = Color.blue;
-            pixCol.a =  0.7f;
-
-            for (int iY = 0; iY < TEXTURE_HEIGHT; iY++)
-            {
-                // Jedem pixel ein Farbwert zugeteilt
+            for (int iY = 0; iY < TEXTURE_HEIGHT; iY++) {
                 tfCols[iX + iY * TEXTURE_WIDTH] = pixCol;
             }
         }
 
-        // in Texture einbetten und anwenden
         texture.wrapMode = TextureWrapMode.Clamp;
         texture.SetPixels(tfCols);
         texture.Apply();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
--- Assets/Scripts/UI/TransferFunction/TransferFunctionUI.cs
+++ Assets/Scripts/UI/TransferFunction/TransferFunctionUI.cs
@@ -98,7 +98,7 @@ public class TransferFunctionUI : MonoBehaviour, IPointerClickHandler {
 
         m_LineRenderer.gameObject.SetActive(true);
 
-        GenerateRandomControlPoints();
+        GenerateStartingPoints();
     }
 
     private void DeselectPoint() {
@@ -191,23 +191,37 @@ public class TransferFunctionUI : MonoBehaviour, IPointerClickHandler {
         return function;
     }
 
-    private void GenerateRandomControlPoints() {
+    private void GenerateStartingPoints() {
         Bounds bounds = RectTransformUtility.CalculateRelativeRectTransformBounds(m_RectTransform);
 
-        const int NUMBER_OF_POINTS = 10;
+        // Coordinates are relativ to the middle center point of the parent rect transform
 
-        float xStep = (bounds.size.x - m_ControlPointSize.x) / NUMBER_OF_POINTS;
-        float x = xStep / 2f;
-        for (int i = 0; i < NUMBER_OF_POINTS; i++) {
-            float y = Random.Range(0, bounds.size.y - m_ControlPointSize.y);
+        float xMin = -bounds.extents.x + (m_ControlPointSize.x / 2f);
+        float xMax =  bounds.extents.x + (m_ControlPointSize.x / 2f);
+        float yMin = -bounds.extents.y + (m_ControlPointSize.y / 2f);
+        float yMax =  bounds.extents.y + (m_ControlPointSize.y / 2f);
 
-            float xPos = x - bounds.extents.x + m_ControlPointSize.x / 2f;
-            float yPos = y - bounds.extents.y + m_ControlPointSize.y / 2f;
+        float x, y;
 
-            CreatePoint(new Vector2(xPos, yPos), Random.ColorHSV(), false);
+        // Blue
+        x = Mathf.Lerp(xMin, xMax, 0.125f);
+        y = Mathf.Lerp(yMin, yMax, 0.25f);
+        CreatePoint(new Vector2(x, y), Color.blue, false);
 
-            x += xStep;
-        }
+        // Green
+        x = Mathf.Lerp(xMin, xMax, 0.375f);
+        y = Mathf.Lerp(yMin, yMax, 0.5f);
+        CreatePoint(new Vector2(x, y), Color.green, false);
+
+        // Yellow
+        x = Mathf.Lerp(xMin, xMax, 0.625f);
+        y = Mathf.Lerp(yMin, yMax, 0.625f);
+        CreatePoint(new Vector2(x, y), Color.yellow, false);
+
+        // Red
+        x = Mathf.Lerp(xMin, xMax, 0.875f);
+        y = Mathf.Lerp(yMin, yMax, 0.75f);
+        CreatePoint(new Vector2(x, y), Color.red, false);
     }
 
     private void OnColorPickerChanged(Color color) {
@@ -267,91 +281,5 @@ public class TransferFunctionUI : MonoBehaviour, IPointerClickHandler {
     private Vector2 ConvertToLocalPointInBox(Vector3 position) {
         RectTransformUtility.ScreenPointToLocalPointInRectangle(m_RectTransform, position, null, out Vector2 localPoint);
         return localPoint;
-    }
-
-    public class TransferFunction {
-        public List<TFColourControlPoint> colourControlPoints = new List<TFColourControlPoint>();
-        public List<TFAlphaControlPoint> alphaControlPoints = new List<TFAlphaControlPoint>();
-
-        public Texture2D histogramTexture = null;
-
-        private Texture2D texture = null;
-        Color[] tfCols;
-
-        private const int TEXTURE_WIDTH = 512;
-        private const int TEXTURE_HEIGHT = 2;
-
-        public TransferFunction() {
-            texture = new Texture2D(TEXTURE_WIDTH, TEXTURE_HEIGHT, TextureFormat.RGBAFloat, false);
-            tfCols = new Color[TEXTURE_WIDTH * TEXTURE_HEIGHT];
-        }
-
-        public void AddControlPoint(TFColourControlPoint ctrlPoint) {
-            colourControlPoints.Add(ctrlPoint);
-        }
-
-        public void AddControlPoint(TFAlphaControlPoint ctrlPoint) {
-            alphaControlPoints.Add(ctrlPoint);
-        }
-
-        public Texture2D GetTexture() {
-            if (texture == null)
-                GenerateTexture();
-
-            return texture;
-        }
-
-        public void GenerateTexture() {
-            List<TFColourControlPoint> cols = new List<TFColourControlPoint>(colourControlPoints);
-            List<TFAlphaControlPoint> alphas = new List<TFAlphaControlPoint>(alphaControlPoints);
-
-            // Sort lists of control points
-            cols.Sort((a, b) => (a.dataValue.CompareTo(b.dataValue)));
-            alphas.Sort((a, b) => (a.dataValue.CompareTo(b.dataValue)));
-
-            // Add colour points at beginning and end
-            if (cols.Count == 0 || cols[cols.Count - 1].dataValue < 1.0f)
-                cols.Add(new TFColourControlPoint(1.0f, Color.white));
-            if (cols[0].dataValue > 0.0f)
-                cols.Insert(0, new TFColourControlPoint(0.0f, Color.white));
-
-            // Add alpha points at beginning and end
-            if (alphas.Count == 0 || alphas[alphas.Count - 1].dataValue < 1.0f)
-                alphas.Add(new TFAlphaControlPoint(1.0f, 1.0f));
-            if (alphas[0].dataValue > 0.0f)
-                alphas.Insert(0, new TFAlphaControlPoint(0.0f, 0.0f));
-
-            int numColours = cols.Count;
-            int numAlphas = alphas.Count;
-            int iCurrColour = 0;
-            int iCurrAlpha = 0;
-
-            for (int iX = 0; iX < TEXTURE_WIDTH; iX++) {
-                float t = iX / (float)(TEXTURE_WIDTH - 1);
-                while (iCurrColour < numColours - 2 && cols[iCurrColour + 1].dataValue < t)
-                    iCurrColour++;
-                while (iCurrAlpha < numAlphas - 2 && alphas[iCurrAlpha + 1].dataValue < t)
-                    iCurrAlpha++;
-
-                TFColourControlPoint leftCol = cols[iCurrColour];
-                TFColourControlPoint rightCol = cols[iCurrColour + 1];
-                TFAlphaControlPoint leftAlpha = alphas[iCurrAlpha];
-                TFAlphaControlPoint rightAlpha = alphas[iCurrAlpha + 1];
-
-                float tCol = (Mathf.Clamp(t, leftCol.dataValue, rightCol.dataValue) - leftCol.dataValue) / (rightCol.dataValue - leftCol.dataValue);
-                float tAlpha = (Mathf.Clamp(t, leftAlpha.dataValue, rightAlpha.dataValue) - leftAlpha.dataValue) / (rightAlpha.dataValue - leftAlpha.dataValue);
-
-                Color pixCol = rightCol.colourValue * tCol + leftCol.colourValue * (1.0f - tCol);
-                pixCol.a = rightAlpha.alphaValue * tAlpha + leftAlpha.alphaValue * (1.0f - tAlpha);
-
-                for (int iY = 0; iY < TEXTURE_HEIGHT; iY++) {
-                    tfCols[iX + iY * TEXTURE_WIDTH] = pixCol;
-                }
-            }
-
-            texture.wrapMode = TextureWrapMode.Clamp;
-            texture.SetPixels(tfCols);
-            texture.Apply();
-        }
     }
 }
