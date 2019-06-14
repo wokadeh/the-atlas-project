79b757aa784ff2bbf7a3da8ea4c04bd3599d61a6
Jan Storm <jan.storm@study.hs-duesseldorf.de>
2019-06-14 20:31:16 +0200

    Add simple loading screen when importing data

--- Assets/Scripts/UI/MenuBar/MenuBarUI.cs
+++ Assets/Scripts/UI/MenuBar/MenuBarUI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 using SFB;
 
@@ -7,6 +8,7 @@ public class MenuBarUI : MonoBehaviour {
     [SerializeField] private VolumeRenderer m_VolumeRenderer;
     [SerializeField] private TransferFunctionUI m_TransferFunctionUI;
     [SerializeField] private Button m_ImportDataButton;
+    [SerializeField] private GameObject m_LoadingScreen;
 
     private void Start() {
         m_ImportDataButton.onClick.AddListener(ImportData);
@@ -15,10 +17,21 @@ public class MenuBarUI : MonoBehaviour {
     private void ImportData() {
         string[] folders = StandaloneFileBrowser.OpenFolderPanel(null, null, false);
         if (folders.Length == 1) {
-            string folder = folders[0];
-            DataAsset data = m_DataManager.Load(folder);
-            m_VolumeRenderer.SetData(data);
-            m_TransferFunctionUI.Redraw();
+            StartCoroutine(ImportDataCoroutine(folders[0]));
         }
+    }
+
+    private IEnumerator ImportDataCoroutine(string folder) {
+        m_LoadingScreen.SetActive(true);
+
+        // We are waiting for two frames so that unity has time to redraw the ui
+        yield return null;
+        yield return null;
+
+        DataAsset data = m_DataManager.Load(folder);
+        m_VolumeRenderer.SetData(data);
+        m_TransferFunctionUI.Redraw();
+
+        m_LoadingScreen.SetActive(false);
     }
 }
