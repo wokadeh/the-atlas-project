b37a23b205e2de52d36fec35d280ca1a111146cf
M.Winter <chas3r@hotmail.de>
2019-07-06 20:40:57 +0200

    Added Change Render Mode Functionality

---
+++ Assets/Scripts/PrepareRenderMode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PrepareRenderMode : MonoBehaviour
+{
+    [SerializeField] private VolumeRenderer m_VolumeRenderer;
+    [SerializeField] private GameObject m_Groundplane;
+    [SerializeField] private GameObject m_EarthSphere;
+
+
+    void LateUpdate()
+    {
+        MeshRenderer ground = m_Groundplane.GetComponent<MeshRenderer>();
+        MeshRenderer sphere = m_EarthSphere.GetComponent<MeshRenderer>();
+
+        if (m_VolumeRenderer.Mode == VolumeRendererMode.Cartesian)
+        {
+            ground.enabled = true;
+            sphere.enabled = false;
+            m_VolumeRenderer.GetComponent<MeshRenderer>().material.shader = Shader.Find("Custom/Ray Casting");
+            m_VolumeRenderer.transform.rotation = Quaternion.identity;
+            m_VolumeRenderer.transform.rotation = Quaternion.Euler(180, 180, 0);
+            m_VolumeRenderer.transform.localScale = new Vector3(1f, 0.2f, 0.75f);
+
+        }else if (m_VolumeRenderer.Mode == VolumeRendererMode.Spherical)
+        {
+            ground.enabled = false;
+            sphere.enabled = true;
+            m_VolumeRenderer.GetComponent<MeshRenderer>().material.shader = Shader.Find("Custom/Spherical Ray Casting");
+            m_VolumeRenderer.transform.localScale = new Vector3(1f, 1f, 1f);
+            m_VolumeRenderer.transform.rotation = Quaternion.identity;
+            m_VolumeRenderer.transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+    }
+}
