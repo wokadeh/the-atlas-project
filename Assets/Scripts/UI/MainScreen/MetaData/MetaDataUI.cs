4deda06698b04d45a30ba9895ef3e0cf1d8b4df3
AERO <alexander.wollert@hs-duesseldorf.de>
2019-08-28 15:42:38 +0200

    Introduce MetaData information panel. No content,  but button trigger exists

---
+++ Assets/Scripts/UI/MainScreen/MetaData/MetaDataUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetaDataUI : MonoBehaviour
+{
+    private GameObject m_MetaDataLabel;
+
+    DataManager m_DataManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_DataManager = GameObject.Find( "SCRIPTS" ).GetComponent<DataManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/TogglePanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Little helper panel that sets a panel active and inactive
+public class TogglePanel : MonoBehaviour
+{
+    public void Toggle( GameObject panel )
+    {
+        panel.SetActive( !panel.activeSelf );
+    }
+}
