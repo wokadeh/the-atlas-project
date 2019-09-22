6b0317393302fad613eaaf233a58c42dde967eed
AERO <alexander.wollert@hs-duesseldorf.de>
2019-09-22 21:20:26 +0200

    Load dynamically all buttons that are children of LeftBar

--- Assets/Scripts/UI/ProjectScreen/ProjectScreenUI.cs
+++ Assets/Scripts/UI/ProjectScreen/ProjectScreenUI.cs
@@ -13,15 +13,12 @@ public class ProjectScreenUI : MonoBehaviour
     [SerializeField] private Button m_ProjectButton;
     [SerializeField] private GameObject m_ProjectScreen;
     [SerializeField] private GameObject m_LeftBarUI;
-    [SerializeField] private Button m_MetaDataButton;
-    [SerializeField] private Button m_SnapshotButton;
 
     private List<Button> m_LeftBarButtonList;
 
     // Start is called before the first frame update
     private void Start()
     {
-        Log.Info( this, "its started" );
         m_ProjectButton.onClick.AddListener(ShowProjectScreen);
     }
 
@@ -48,11 +45,16 @@ public class ProjectScreenUI : MonoBehaviour
 
     private List<Button> GetButtonList()
     {
-        if(m_LeftBarButtonList == null)
+        if(m_LeftBarButtonList == null || m_LeftBarButtonList.Count == 0)
         {
             m_LeftBarButtonList = new List<Button>();
-            m_LeftBarButtonList.Add( m_MetaDataButton );
-            m_LeftBarButtonList.Add( m_SnapshotButton );
+            for( int i = 0; i < m_LeftBarUI.transform.childCount; i++ )
+            {
+                if( m_LeftBarUI.transform.GetChild( i ).GetComponent<Button>() != null )
+                {
+                    m_LeftBarButtonList.Add( m_LeftBarUI.transform.GetChild( i ).GetComponent<Button>() );
+                }
+            }
             return m_LeftBarButtonList;
         }
         else
