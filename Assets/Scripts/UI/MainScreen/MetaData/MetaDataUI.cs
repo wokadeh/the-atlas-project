f46bfbef609bb247f5ada25646e9382e423ba3c2
AERO <alexander.wollert@hs-duesseldorf.de>
2019-09-22 15:11:02 +0200

    Rearrange icons on screen and disable them on start

--- Assets/Scripts/UI/MainScreen/MetaData/MetaDataUI.cs
+++ Assets/Scripts/UI/MainScreen/MetaData/MetaDataUI.cs
@@ -1,23 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 
 public class MetaDataUI : MonoBehaviour
 {
-    private GameObject m_MetaDataLabel;
+    private TextMeshProUGUI m_Text;
 
     DataManager m_DataManager;
-
 
     // Start is called before the first frame update
     void Start()
     {
         m_DataManager = GameObject.Find( "SCRIPTS" ).GetComponent<DataManager>();
+        m_Text = this.transform.GetChild( 0 ).GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        this.ReadWriteData();
+    }
+
+    private void ReadWriteData()
+    {
+        m_Text.text = "";
+        // Start time
+        m_Text.text += "\nStart time:\t" + m_DataManager.MetaData.StartDateTimeNumber.ToString();
+        // End time
+        m_Text.text += "\nEnd time:\t\t" + m_DataManager.MetaData.EndDateTimeNumber.ToString();
+        // List parameters
+        m_Text.text += "\nVariables:\t\t";
+        for(int i = 0; i < m_DataManager.MetaData.Variables.Count; i++)
+        {
+            m_Text.text += m_DataManager.MetaData.Variables[i].Name;
+
+            if( i != m_DataManager.MetaData.Variables.Count - 1 ) m_Text.text += ", ";
+        }
+        // Number of levels
+        m_Text.text += "\nAlt. pres. levels:\t\t" + m_DataManager.MetaData.Levels.ToString();
+        // Hour intervals
+        m_Text.text += "\nHourly interval:\t\t" + m_DataManager.MetaData.TimeInterval.ToString();
+        // Bit depth
+        m_Text.text += "\nBit depth:\t\t" + m_DataManager.MetaData.BitDepth.ToString();
     }
 }
