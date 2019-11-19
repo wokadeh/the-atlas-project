d0e6e94d20f8832b44fc08f393349b585f04c620
ATLAS <cg.medien@hs-duesseldorf.de>
2019-11-19 14:17:45 +0100

    Move Cartesian/Spherical button to one activation button into the left bar

---
+++ Assets/Scripts/UI/MainScreen/ModeUI.cs
@@ -0,0 +1,27 @@
+﻿// ****************************** LOCATION ********************************
+//
+// [UI] Activate_Spherical -> attached
+//
+// ************************************************************************
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModeUI : MonoBehaviour {
+
+    [SerializeField] private Button m_SphericalActivateButton;
+
+    private void Start()
+    {
+        m_SphericalActivateButton.onClick.AddListener( () => {
+            if( Singleton.GetVolumeRenderer().Mode != VolumeRendererMode.Cartesian )
+            {
+                Singleton.GetVolumeRenderer().SetMode( VolumeRendererMode.Cartesian );
+            }
+            else
+            {
+                Singleton.GetVolumeRenderer().SetMode( VolumeRendererMode.Spherical );
+            }
+        } );
+    }
+}
